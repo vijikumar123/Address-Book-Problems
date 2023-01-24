c377d3411165584724d9cfcac531cda4459391bd
vijikumar123 <vk9164940756@gmail.com>
2023-01-24 14:12:32 +0530

    added: uc5 Address boook problems

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,47 +1,96 @@
 package com.bridgelabz.addressbook;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class AddressBook {
-    PersonDetails obj = new PersonDetails();
-    int count=0;
-    String arr[] = new String[70];
-    public void saveAddress() {
+    ArrayList<PersonDetails> contactList = new ArrayList<>();
+
+    public void addContact() {
+        PersonDetails contactPerson = new PersonDetails();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your contacts ");
-        System.out.print("Name: ");
-        arr[count++]=sc.nextLine();
-        System.out.print("Surname: ");
-        arr[count++]=sc.nextLine();
-        System.out.print("Address: ");
-        arr[count++]=sc.nextLine();
-        System.out.print("City: ");
-        arr[count++]=sc.nextLine();
-        System.out.print("State: ");
-        arr[count++]=sc.nextLine();
-        System.out.print("ContactNumber: ");
-        arr[count++]=sc.nextLine();
-        System.out.print("Email: ");
-        arr[count++]=sc.nextLine();
+        System.out.println("Enter the details of contact person");
+        System.out.print("Enter first name:");
+        contactPerson.setName(sc.next());
+        System.out.print("Enter Last name:");
+        contactPerson.setSurName(sc.next());
+        System.out.print("Enter the Address : ");
+        contactPerson.setAddress(sc.next());
+        System.out.print("Enter the City : ");
+        contactPerson.setCity(sc.next());
+        System.out.print("Enter the State : ");
+        System.out.println();
+        contactPerson.setState(sc.next());
+        System.out.print("Enter the ZipCode : ");
+        contactPerson.setZip(sc.next());
+        System.out.print("Enter the Mobile no : ");
+        contactPerson.setContactNumber(sc.next());
+        contactList.add(contactPerson);
     }
-    public void showMyAddress(){
-        int count =1;
-        for(String add : arr){
-            if (add!=null && !add.isEmpty()){
-                System.out.print(add+", ");
-                count++;
-            }
-            if(count==8){
-                System.out.println();
-                count=1;
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "contactList=" + contactList +
+                '}';
+    }
+
+    public void editContact() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first name:");
+        String name = sc.next();
+        for (PersonDetails contactPerson : contactList) {
+            if (name.equals(contactPerson.getName())) {
+                System.out.println("What you want to edit for the contact");
+                System.out.println("1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Zip Code\n7." +
+                        "Mobile Number\n8.Email id");
+                int choice = sc.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter the First Name :");
+                        contactPerson.setName(sc.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter the Last Name :");
+                        contactPerson.setSurName(sc.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter the Address :");
+                        contactPerson.setAddress(sc.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter the City :");
+                        contactPerson.setCity(sc.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter the State :");
+                        contactPerson.setState(sc.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter the Zip code :");
+                        contactPerson.setZip(sc.next());
+                        break;
+                    case 7:
+                        System.out.println("Enter the Mobile Number :");
+                        contactPerson.setContactNumber(sc.next());
+                        break;
+                }
             }
         }
-
     }
 
-    public void saveAddress1() {
-
+    public void deleteContact() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first name:");
+        String name = sc.next();
+        for (PersonDetails contactPerson : contactList) {
+            if (name.equals(contactPerson.getName())) {
+                contactList.remove(contactPerson);
+                break;
+            }
+        }
     }
 }
+
 
 
 
