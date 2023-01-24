fb32262333e744c368589cb02e9e1ac8e06e6135
vijikumar123 <vk9164940756@gmail.com>
2023-01-24 13:51:42 +0530

    added: uc1 Address boook problems

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -1,7 +1,69 @@
 package com.bridgelabz.addressbook;
 
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("hello java");
+    private String name ;
+    private String surname ;
+    private String address;
+    private String city;
+    private String state;
+    private String contactNumber;
+    private String email;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
+
